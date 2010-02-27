8ec039b483248c10b4be02009a536f6be4a384bb
chakrit <chakrit@19bb3992-ce45-2342-9e5f-15edef2645fe>
2010-02-27 20:33:25 +0000

    - Removed a bunch of unused files

--- src/TalkCalc/HTK/compile.bat
+++ src/TalkCalc/HTK/compile.bat
@@ -6,3 +6,8 @@ bin\HParse in\calc.gram out\calc.wdnet
 
 rem HTK requires the pronunciation file to be sorted
 sort in\calc.dict > out\calc.dict
+
+rem Copy configuration files over
+copy in\liverecog.config out\liverecog.config
+copy in\thai_acoustic_model out\thai_acoustic_model
+copy in\tie.list out\tie.list
--- src/TalkCalc/HTK/gen_training_set.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-IF NOT EXIST train MKDIR train
-bin\HSGen -l -n 200 out\calc.wdnet out\calc.dict > train\prompts.txt
