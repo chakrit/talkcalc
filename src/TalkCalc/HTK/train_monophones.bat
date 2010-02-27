4355ef009ef8f9b99de4356f79aeb6d4f89a1d9f
chakrit <chakrit@19bb3992-ce45-2342-9e5f-15edef2645fe>
2010-02-27 20:07:03 +0000

    - Save progress

--- src/TalkCalc/HTK/train_monophones.bat
+++ src/TalkCalc/HTK/train_monophones.bat
@@ -6,6 +6,8 @@ IF NOT EXIST out\am\hmm_0 MKDIR out\am\hmm_0
 
 CALL compile.bat
 
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 rem Extract audio parameters via HCopy
 bin\HCopy -T 1 -C in\code.config -S in\code.scp
 
@@ -14,5 +16,19 @@ bin\HCompV -T 1 -C in\train.config -f 0.01 -m -S in\train.scp -M out\am\hmm_0 in
 
 rem Copy the prototype for each phonemes so we can train each of them
 for /F %%f in (in\monophones.list) do (
-	bin\sed s/'hmm_proto'/'%%f'/ out\am\hmm_0\hmm_proto > "out\am\hmm_0\%%f"
+	bin\replacetext hmm_proto "%%f" < out\am\hmm_0\hmm_proto > "out\am\hmm_0\%%f"
 )
+
+rem Calculate the probabilities for each phonemes and combine them into newMacros
+for /L %%i in (1,1,3) do (
+	IF NOT EXIST out\am\hmm_%%i MKDIR out\am\hmm_%%i
+	IF %%i==1 (
+		bin\HERest -T 1 -A -C in\train.config -I in\monophones.mlf -S in\train.scp -d out\am\hmm_0 -M out\am\hmm_1 in\monophones.list
+	) ELSE (
+		SET /A htk__j=%%i-1
+		bin\HERest -T 1 -A -C in\train.config -I in\monophones.mlf -S in\train.scp -H out\am\hmm_!htk__j!\newMacros -M out\am\hmm_%%i in\monophones.list
+	)
+)
+
+
+ENDLOCAL
