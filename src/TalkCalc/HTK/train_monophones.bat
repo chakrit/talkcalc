cb2a8ece73da3899aa87495ba890abb89b4e51b4
chakrit <chakrit@19bb3992-ce45-2342-9e5f-15edef2645fe>
2010-02-27 19:37:15 +0000

    - Save progress - Added a text replacement tool because sed is unreliable

--- src/TalkCalc/HTK/train_monophones.bat
+++ src/TalkCalc/HTK/train_monophones.bat
@@ -9,5 +9,10 @@ CALL compile.bat
 rem Extract audio parameters via HCopy
 bin\HCopy -T 1 -C in\code.config -S in\code.scp
 
-rem Train the monophones
+rem Create a prototype HMM for acoustic model training
 bin\HCompV -T 1 -C in\train.config -f 0.01 -m -S in\train.scp -M out\am\hmm_0 in\hmm_proto
+
+rem Copy the prototype for each phonemes so we can train each of them
+for /F %%f in (in\monophones.list) do (
+	bin\sed s/'hmm_proto'/'%%f'/ out\am\hmm_0\hmm_proto > "out\am\hmm_0\%%f"
+)
