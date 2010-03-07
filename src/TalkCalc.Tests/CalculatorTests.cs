5a08518e7b9502c14907959eeaa9af54a20455e5
chakrit <chakrit@19bb3992-ce45-2342-9e5f-15edef2645fe>
2010-03-07 05:56:19 +0000

    - Added some tests for the calculator to make sure it works as expected.

---
+++ src/TalkCalc.Tests/CalculatorTests.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TalkCalc.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void NullStringShouldEvaluateToZero()
+        {
+            test(null, 0F);
+            test(string.Empty, 0F);
+        }
+
+        [TestMethod]
+        public void SimpleDigitShouldEvaluateToSelf()
+        {
+            for (var i = 0; i < 10; i++)
+                test(i.ToString(), (float)i);
+        }
+
+        [TestMethod]
+        public void NumbersShouldEvaluateToSelf()
+        {
+            for (var i = 1; i < 10000; i *= 2)
+                test(i.ToString(), (float)i);
+        }
+
+        [TestMethod]
+        public void SingleOperationShouldEvaluateCorrectly()
+        {
+            test("1 + 1", 2F);
+            test("98 - 89", -9F);
+            test("7 * 7", 49F);
+            test("888 / 888", 1F);
+            test("9 - 5", -4F);
+        }
+
+        [TestMethod]
+        public void MultipleOperationShouldEvaluateInCorrectOrder()
+        {
+            test("4 + 5 * 6", 34F);
+            test("5 * 6 + 4", 34F);
+        }
+
+
+
+
+        private void test(string expr, float expectedResult)
+        {
+            var calc = new Calculator.Calculator();
+            calc.Expression = expr;
+
+            var result = calc.Calculate();
+            Assert.AreEqual(result, calc.Result);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
